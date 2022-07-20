44140714f34c3edef4ec37fab809f50052fe3987
OlgaTitarenko <kizuneworld@gmail.com>
2022-07-20 20:51:42 +0300

    add translate

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Fond
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = [:en, :ua]
+    config.i18n.default_locale = :ua
   end
 end
